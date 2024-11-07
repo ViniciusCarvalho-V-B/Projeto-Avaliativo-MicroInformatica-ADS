78b525244b8c3af57f4624039d3746be4a9a7a75
Vinicius <viniciuscvb3@gmail.com>
2024-11-06 23:49:06 -0300

    Alteração Final 06-11-2024 23:50
    adicionado api do googlemaps na pagina de contatos e mais alguns ajustes gerais

---
+++ Js/app.js
@@ -0,0 +1,32 @@
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+        
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div');
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('');
+
+  alertPlaceholder.innerHTML = '';
+  alertPlaceholder.append(wrapper);
+}
+
+let alertTrigger = document.getElementById('liveAlertBtn');
+
+if (alertTrigger !== null) {
+  alertTrigger.addEventListener('click', () => {
+    let nome = document.getElementById('nome').value.trim();
+    let email = document.getElementById('email').value.trim();
+    let whatsapp = document.getElementById('whatsapp').value.trim();
+    let message = document.getElementById('message').value.trim();
+    let estado = document.getElementById('estado').value.trim();
+    
+    if (nome && email && whatsapp && message && estado) {
+      appendAlert('Sua mensagem foi enviada com sucesso! Obrigado por entrar em contato conosco!', 'success');
+    } else {
+      appendAlert('Por favor, preencha todos os campos obrigatórios.', 'danger');
+    }
+  });
+}
